62582bbe805b869389a7c09d60defd18d03a4482
@hexlet/cli <support@hexlet.io>
2023-04-11 21:15:30 +0300

    submit java-collections-ru/lambdas

---
+++ java-collections-ru/lambdas/src/main/java/exercise/App.java
@@ -0,0 +1,16 @@
+package exercise;
+
+import java.util.Arrays;
+
+// BEGIN
+class App{
+
+    public static String[][] enlargeArrayImage(String[][]image){
+        // надо создать дополнительный массив, чтобы увеличить ?
+        String[][] doubleArray = new String[2][1];
+
+        return doubleArray;
+
+    }
+        }
+// END
