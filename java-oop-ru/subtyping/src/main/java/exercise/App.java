e83f1894f9105d8ab2f6a1684ed4e12cd7f6ccb6
NikitaPoznan <pz.nikita2015@yandex.ru>
2023-06-10 13:39:43 +0300

    Exercise 4 (OOP)

--- java-oop-ru/subtyping/src/main/java/exercise/App.java
+++ java-oop-ru/subtyping/src/main/java/exercise/App.java
@@ -1,9 +1,15 @@
 package exercise;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
 // BEGIN
-
+public class App {
+    public static void swapKeyValue(KeyValueStorage kv) {
+        Map<String, String> newData = new HashMap<>();
+        for()
+    }
+}
 // END
--- java-oop-ru/subtyping/src/main/java/exercise/InMemoryKV.java
+++ java-oop-ru/subtyping/src/main/java/exercise/InMemoryKV.java
@@ -4,5 +4,33 @@ import java.util.Map;
 import java.util.HashMap;
 
 // BEGIN
+import java.util.Map;
 
+public class InMemoryKV implements KeyValueStorage {
+    private Map<String, String> store;
+
+    public InMemoryKV(Map<String, String> initialStore) {
+        this.store = initialStore;
+    }
+
+    @Override
+    public void set(String key, String value) {
+
+    }
+
+    @Override
+    public void unset(String key) {
+
+    }
+
+    @Override
+    public String get(String key, String defaultValue) {
+        return null;
+    }
+
+    @Override
+    public Map<String, String> toMap() {
+        return null;
+    }
+}
 // END
